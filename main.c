ff46c2b88ce3310b9504e5c3327ed9c34dabc51e
JonatanDZ <jmuhle24@student.aau.dk>
2024-11-07 15:20:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,4 @@
 
 int main(){
     printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
-    printf("Hej");
-    printf("Done");
-    printf("hi");
-    printf("hi");
-    printf("hi");
-    printf("hi");
-    printf("hi");
-    printf("hi");
-    printf("hi");
-    printf("hi");
-    printf("hi");
 }
