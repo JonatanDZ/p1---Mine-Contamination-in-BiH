cb37f829ee03b3f34c78ce7c57e778cf63fda4c9
JonatanDZ <jmuhle24@student.aau.dk>
2024-11-08 14:00:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,4 +4,5 @@ int main(){
     printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
     printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
     printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
+    printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
 }
