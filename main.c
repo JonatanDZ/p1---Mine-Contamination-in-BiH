4b1217b5cab9550f57e894064e99d96b16dd00f9
8ruun <mbja24@student.aau.dk>
2024-11-08 14:17:26 +0100

    Update main.c
    test

--- main.c
+++ main.c
@@ -5,4 +5,5 @@ int main(){
     printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
     printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
     printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
+    printf("Hej");
 }
