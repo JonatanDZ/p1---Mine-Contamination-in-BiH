657d1b23ddaac112fdba1762108fba28e5291387
JonatanDZ <jmuhle24@student.aau.dk>
2024-11-11 14:51:46 +0100

    backup

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+int printMap(int* map);
 
 int main(){
     printf("Hello, from p1---Mine-Contamination-in-BiH!\n");
+}
+
+int printMap(int* map){
+    int length = sizeof(map);
+    for (int i = 0; i < length; i++){
+        for (int j = 0; j < length; j++){
+            printf(" %d ", map[i * length + j]);
+        }
+        printf("\n");
+    }
+    return map;
 }
