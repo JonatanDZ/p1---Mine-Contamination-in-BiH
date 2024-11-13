97fb9de159a442177ad678ecfa3deb229b27af08
JonatanDZ <jmuhle24@student.aau.dk>
2024-11-13 12:58:44 +0100

    Med input til start og slut

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #include "header.h"
 
-
-
 int main(){
-    printf("Hello, from p1---Mine-Contamination-in-BiH!\n\n");
-
     const int mapSize = 32;
     int* map = malloc((mapSize * mapSize) * sizeof(int));
 
     createMap(map, mapSize);
 
+
+    // Starter fra 0, så minus en for koordinat.
+    int startPosistion = 10000;
+    int startInputX;
+    int startInputY;
+
+    printf("Input x og y koordinat for start koordinator");
+    scanf(" %c %c", &startInputX, &startInputY);
+
+    map[startInputX * mapSize + startInputY] = startPosistion;
+
+    int endPosistion = 10000;
+    int endInputX;
+    int endInputY;
+
+    printf("Input x og y koordinat for end koordinator");
+    scanf(" %c%c", &endInputX, &endInputY);
+
+    map[endInputX * mapSize + endInputY] = endPosistion;
+    
     printMap(map, mapSize);
+
 
     return 0;
 }
--- printMap.c
+++ printMap.c
@@ -7,7 +7,7 @@
 void printMap(int* map, const int mapSize) { //We are printing the map
     for (int i = 0; i < mapSize; i++){
         for (int j = 0; j < mapSize; j++){
-            printf(" %c ", map[i * mapSize + j]);
+            printf(" %d ", map[i * mapSize + j]);
         }
         printf("\n");
     }
