dc5023c930e84195bdddb5927c2d807969be910f
JonatanDZ <jmuhle24@student.aau.dk>
2024-11-19 11:22:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include "header.h"
 
+#define MAPSIZEROW 32
+#define MAPSIZECOL 32
+
+typedef struct {
+    
+
+}, cell;
+
+
 int main(){
     const int mapSize = 32;
     int* map = malloc((mapSize * mapSize) * sizeof(int));
