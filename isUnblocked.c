e4036e7697d4234709acce2e0914c52aa679ea48
JonatanDZ <jmuhle24@student.aau.dk>
2024-11-29 10:57:23 +0100

    Clean up

--- isUnblocked.c
+++ isUnblocked.c
@@ -2,6 +2,7 @@
 // Created by Jonatan Muhle-Zimino on 20/11/2024.
 //
 #include "header.h"
+//Checker to see if the cell is blocked.
 bool isUnblocked(int map[MAPSIZEROW][MAPSIZECOL], int row, int col) {
     if (map[row][col] == water || map[row][col] == mine) {
         return false;
