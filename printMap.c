2f3ad526b5f594100f801c59728dd2c19c40d447
TimFangel <tfni24@student.aau.dk>
2024-11-29 11:23:06 +0100

    Added comments
    bruh

--- printMap.c
+++ printMap.c
@@ -3,9 +3,14 @@
 //
 
 #include "header.h"
-//prints the map out.
-void printMap(int map[MAPSIZEROW][MAPSIZECOL]) { //We are printing the map
+
+/**
+ * Function that prints the map.
+ * @param map int array from main.
+ */
+void printMap(int map[MAPSIZEROW][MAPSIZECOL]) {
     printf("\n");
+    // For-loop which prints each value in the array.
     for (int i = 0; i < MAPSIZEROW; i++){
         for (int j = 0; j < MAPSIZEROW; j++){
             printf("%2d ", map[i][j]);
