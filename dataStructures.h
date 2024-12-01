663ee9f7e787e2aa78e3550079b0a4083c2afe77
arezva23 <151622443+arezva23@users.noreply.github.com>
2024-12-01 22:17:02 +0100

    1

--- dataStructures.h
+++ dataStructures.h
@@ -9,8 +9,22 @@
 
 #include "header.h"
 
-struct node_t {
+/**
+ * A struct for one component of the linked list. Next is NULL if no next ecists
+ */
+typedef struct node_t { //Needs name here...
     cell_t* cell;
-    struct node_t* next;
-};
+    struct node_t* next; //so it can be used here
+} node_t;
+
+/**
+ * The type for the list itself. In actuality it is simply a pointer to the head of the list.
+ */
+typedef struct {
+    node_t* head;
+} list_t;
+
+
+
+
 
