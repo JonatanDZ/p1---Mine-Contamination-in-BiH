c6e195893433fa1f72a8c64a0c3e683f88173de5
arezva23 <151622443+arezva23@users.noreply.github.com>
2024-12-02 15:19:33 +0100

    2

--- dataStructures.c
+++ dataStructures.c
@@ -4,6 +4,11 @@
 
 #include "dataStructures.h"
 
-void addItem(list_t* list, cell_t* cell) {
-    listNode_t* newNode = (listNode_t*)malloc(sizeof(listNode_t));
+void addCell(list_t* list, cell_t* cell) {
+    listNode_t* newNode = (listNode_t*) malloc(sizeof(listNode_t));
+    newNode->cell = cell;
+    newNode->next = list->head; //Make the current list head the 'next' of this node
+    list->head = newNode;       //Update the head of the list to this node
 }
+
+void deleteCell
