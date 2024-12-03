217f6ef954174a58da1b173ef8a6431f9d666df5
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2024-12-03 12:17:39 +0100

    Comments update

--- main.c
+++ main.c
@@ -4,23 +4,25 @@ int main(){
     coor_t start, dest;
     int map [MAPSIZEROW][MAPSIZECOL];
 
+    //Map with a file
     //createMapWFile(map);
-
     //fromNumberToEnum(map);
 
+    //map from hardcode.
     createMap(map);
 
-
+    //input for the shortest route and random mine gen functions.
     inputForShortestRouteAndMineGen(map);
-
     input(map, &start, &dest);
 
     //aStarSearch(map, start, dest);
 
+    //print map without special char
     printMap(map);
 
-
+    //converts the normal map to specefic chars.
     terminalOutPut(map);
+    //prints the new map with special chars.
     printMapChar(map);
 
 
