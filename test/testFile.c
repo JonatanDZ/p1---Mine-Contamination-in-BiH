2fda4f78a22a242e4abfa63453f5af6aa40f6e7a
8ruun <mbja24@student.aau.dk>
2024-12-04 11:30:59 +0100

    hej

--- test/testFile.c
+++ test/testFile.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 
-bool test_isWithinArray(void) {
+bool test_isWithinArray5_5(void) {
     //ARRANGE, from input
     int test_row = 5;
     int test_col = 5;
@@ -17,9 +17,25 @@ bool test_isWithinArray(void) {
     return result;
 }
 
+bool test_isWithinArray7_55(void) {
+    //ARRANGE, from input
+    int test_row = 7;
+    int test_col = 55;
+
+    //ACT
+    bool result = isWithinArray(test_row,test_col);
+
+    //ASSERT
+    assert(result == false);
+
+    return result;
+}
+
+
+
 
 
 int main(void) {
-    printf("%d",test_isWithinArray());
-
+    printf("Is Within Array 5,5: %d",test_isWithinArray5_5());
+    printf("Is Within Array 7,55: %d",test_isWithinArray7_55());
 }
