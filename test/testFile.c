f11101bad054c9fb538e457a0f014cb09636fdc9
bkljucic <bkljuc24@student.aau.dk>
2024-12-04 14:55:03 +0100

    lille is unblocked

--- test/testFile.c
+++ test/testFile.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include "../src/map.h"
 #include <stdio.h>
+#include "../src/input.h"
 
 
 bool test_isWithinArray5_5(void) {
@@ -31,11 +32,31 @@ bool test_isWithinArray7_55(void) {
     return result;
 }
 
+bool test_isUnblocked(void) {
+    //ARRANGE
+    int test_row = 0;
+    int test_col = 5;
+    int map[5][5] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, forest, forest, water},
+        {asphalt, forest, forest, water, water},
+        {forest, asphalt, asphalt, water, forest}
+        };
+    //ACT
+    bool result = isUnblocked(map, test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+
+    return result;
+}
 
 
 
 
 int main(void) {
-    printf("Is Within Array 5,5: %d",test_isWithinArray5_5());
-    printf("Is Within Array 7,55: %d",test_isWithinArray7_55());
+    printf("Is Within Array 5,5: %d\n",test_isWithinArray5_5());
+    printf("Is Within Array 7,55: %d\n",test_isWithinArray7_55());
+    printf("Is Unblocked 30,30: %d\n", test_isUnblocked());
 }
