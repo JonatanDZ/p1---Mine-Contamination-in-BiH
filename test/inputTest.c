4133fe9302b9f72626ad847eb7c5a253a339637b
bkljucic <bkljuc24@student.aau.dk>
2024-12-04 11:29:50 +0100

    min lille søde branch om inputtest

--- test/inputTest.c
+++ test/inputTest.c
@@ -1,3 +1,22 @@
-//
-// Created by a-bti on 12/4/2024.
-//
+#include <assert.h>
+#include <stdbool.h>  // For 'false'
+#include "../src/input.h"  // Assuming input.h declares 'input' and 'coor_t'
+
+
+
+void test_error(void);
+
+int main(void) {
+    test_error();
+}
+
+void test_error(void) {
+    int map[MAPSIZEROW][MAPSIZECOL] = {{0}};  // Example 2D array initialization
+    coor_t start = {0, 0};
+    coor_t dest = {1, 1};
+
+    int expected = false;  // Assuming 'false' represents an error code
+
+    int result = input(map, &start, &dest);
+    assert(result == expected);
+}
