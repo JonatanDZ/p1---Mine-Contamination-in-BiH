c94119623d2fac0445fab70465116e4dc3eb7625
arezva23 <151622443+arezva23@users.noreply.github.com>
2024-12-04 11:50:50 +0100

    assertion function

--- test/testFile.c
+++ test/testFile.c
@@ -2,7 +2,7 @@
 #include "../src/map.h"
 #include <stdio.h>
 
-
+bool assertion(void* input, void* expected);
 bool test_isWithinArray(void) {
     //ARRANGE, from input
     int test_row = 5;
@@ -12,12 +12,16 @@ bool test_isWithinArray(void) {
     bool result = isWithinArray(test_row,test_col);
 
     //ASSERT
-    assert(result == true);
-
-    return result;
+    return assertion((bool*)result ,(bool*)true);
 }
 
-
+bool assertion(void* input, void* expected) {
+    if (input == expected) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int main(void) {
     printf("%d",test_isWithinArray());
