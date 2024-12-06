9b70bc1e6eec025057b747a7716dfc2e66646499
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2024-12-06 14:47:39 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/fange/OneDrive - Aalborg Universitet/P1 GitHub/p1---Mine-Contamination-in-BiH/src
-# Build directory: C:/Users/fange/OneDrive - Aalborg Universitet/P1 GitHub/p1---Mine-Contamination-in-BiH/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
