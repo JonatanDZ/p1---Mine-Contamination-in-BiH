33ec55afaa851835bca16c2a97d80a6f85fbad25
arezva23 <151622443+arezva23@users.noreply.github.com>
2024-12-10 12:34:40 +0100

    cleanup

--- test/mapTest.c
+++ test/mapTest.c
@@ -2,54 +2,6 @@
 #include "../src/map.h"
 
 
-void test_isWithinArray_5_5() {
-    //ARRANGE
-    int test_row = 5;
-    int test_col = 5;
-
-    //ACT
-    bool result = isWithinArray(test_row,test_col);
-
-    //ASSERT
-    assert(result == true);
-}
-
-void test_isWithinArray_50_50() {
-    //ARRANGE
-    int test_row = 50;
-    int test_col = 50;
-
-    //ACT
-    bool result = isWithinArray(test_row,test_col);
-
-    //ASSERT
-    assert(result == false);
-}
-
-void test_isWithinArray_neg1_16() {
-    //ARRANGE
-    int test_row = -1;
-    int test_col = 16;
-
-    //ACT
-    bool result = isWithinArray(test_row,test_col);
-
-    //ASSERT
-    assert(result == false);
-}
-
-void test_isWithinArray_19_neg20() {
-    //ARRANGE
-    int test_row = 19;
-    int test_col = -20;
-
-    //ACT
-    bool result = isWithinArray(test_row,test_col);
-
-    //ASSERT
-    assert(result == false);
-}
-
 void test_createMapWFile() {
     //ARRANGE
     int mapHardcoded[MAPSIZEROW][MAPSIZECOL];
@@ -65,4 +17,49 @@ void test_createMapWFile() {
             assert(mapHardcoded[i][j] == mapFromFile[i][j]);
         }
     }
+}
+
+void test_isWithinArray_5_5() {
+    //ARRANGE
+    int test_row = 5;
+    int test_col = 5;
+
+    //ACT
+    bool result = isWithinArray(test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+}
+void test_isWithinArray_50_50() {
+    //ARRANGE
+    int test_row = 50;
+    int test_col = 50;
+
+    //ACT
+    bool result = isWithinArray(test_row,test_col);
+
+    //ASSERT
+    assert(result == false);
+}
+void test_isWithinArray_neg1_16() {
+    //ARRANGE
+    int test_row = -1;
+    int test_col = 16;
+
+    //ACT
+    bool result = isWithinArray(test_row,test_col);
+
+    //ASSERT
+    assert(result == false);
+}
+void test_isWithinArray_19_neg20() {
+    //ARRANGE
+    int test_row = 19;
+    int test_col = -20;
+
+    //ACT
+    bool result = isWithinArray(test_row,test_col);
+
+    //ASSERT
+    assert(result == false);
 }
