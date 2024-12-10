abe27a91bdd40453dabb01ed5897300e78bb557f
arezva23 <151622443+arezva23@users.noreply.github.com>
2024-12-10 12:52:33 +0100

    benjas

--- test/mapTest.c
+++ test/mapTest.c
@@ -63,3 +63,187 @@ void test_isWithinArray_19_neg20() {
     //ASSERT
     assert(result == false);
 }
+
+
+bool test_isUnblocked0_4water(void) {
+    //ARRANGE
+    int test_row = 0;
+    int test_col = 4;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+        };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == false);
+
+    return result;
+}
+
+bool test_isUnblocked0_3forest(void) {
+    //ARRANGE
+    int test_row = 0;
+    int test_col = 3;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+    };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+
+    return result;
+}
+
+bool test_isUnblocked4_2mine(void) {
+    //ARRANGE
+    int test_row = 4;
+    int test_col = 2;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+    };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == false);
+
+    return result;
+}
+
+
+bool test_isUnblocked0_1field(void) {
+    //ARRANGE
+    int test_row = 0;
+    int test_col = 1;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+    };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+
+    return result;
+}
+
+
+
+bool test_isUnblocked1_1asphalt(void) {
+    //ARRANGE
+    int test_row = 1;
+    int test_col = 1;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+    };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+
+    return result;
+}
+
+bool test_isUnblocked4_3mountain1(void) {
+    //ARRANGE
+    int test_row = 4;
+    int test_col = 3;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+    };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+
+    return result;
+}
+
+bool test_isUnblocked4_4mountain2(void) {
+    //ARRANGE
+    int test_row = 4;
+    int test_col = 4;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+    };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+
+    return result;
+}
+
+bool test_isUnblocked2_4startPosition(void) {
+    //ARRANGE
+    int test_row = 2;
+    int test_col = 4;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+    };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+
+    return result;
+}
+
+bool test_isUnblocked3_4endPosition(void) {
+    //ARRANGE
+    int test_row = 3;
+    int test_col = 4;
+    int map2[32][32] = {
+        {water, field, asphalt, forest, water},
+        {water, asphalt, forest, water, water},
+        {forest, asphalt, city, city, startPosition},
+        {asphalt, forest, forest, water, endPosition},
+        {forest, asphalt, mine, mountain1, mountain2}
+    };
+    //ACT
+    bool result = isUnblocked(map2, test_row,test_col);
+
+    //ASSERT
+    assert(result == true);
+
+    return result;
+}
