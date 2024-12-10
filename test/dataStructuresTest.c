6050e01282b5513b9ce558366685284efe9b5132
arezva23 <151622443+arezva23@users.noreply.github.com>
2024-12-10 17:41:44 +0100

    fixed fejl, passer nu

--- test/dataStructuresTest.c
+++ test/dataStructuresTest.c
@@ -5,14 +5,14 @@
 void test_initEmptyCellMap() {
     //Arrange
     cell_t emptyCellMap[MAPSIZEROW][MAPSIZECOL];
-    double tolerance = 0.1;
+    double tolerance = 0.01;
 
     //Act
     initEmptyCellMap(emptyCellMap);
 
     for (int i = 0; i < MAPSIZEROW; i++) {
         for (int j = 0; j < MAPSIZECOL; j++) {
-            /*
+
             assert(emptyCellMap[i][j].f - DBL_MAX < tolerance);
             assert(emptyCellMap[i][j].g - DBL_MAX < tolerance);
             assert(emptyCellMap[i][j].h - DBL_MAX < tolerance);
@@ -22,7 +22,7 @@ void test_initEmptyCellMap() {
 
             assert(emptyCellMap[i][j].openList == false);
             assert(emptyCellMap[i][j].closedList == false);
-            */
+
         }
     }
 
